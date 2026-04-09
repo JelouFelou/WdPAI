171f9f8aaec6676d860f99c81d0b90d6aa2c4868
JelouFelou <adr-bober@o2.pl>
2026-04-09 16:55:59 +0200

    More accurate dashboard to Prototype

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class AppController {
     protected function isGet(): bool
     {
@@ -12,27 +11,46 @@ class AppController {
         return $_SERVER["REQUEST_METHOD"] === 'POST';
     }
  
-    protected function render(string $template = null, array $variables = [])
+    protected function render(?string $template = null, array $variables = [])
     {
-        $templatePath = 'public/views/'. $template.'.html';
-        $templatePath404 = 'public/views/404.html';
-        $output = "";
-                 
+        // Określamy ścieżkę bazową do widoków
+        $basePath = __DIR__.'/../../public/views/';
+        
+        $templatePath = $basePath . $template . '.html';
+        $headPath = $basePath . 'partials/head.html';
+        $navPath = $basePath . 'partials/nav.html';
+        
         if(file_exists($templatePath)){
             extract($variables);
-            // ["tab_name" => $title]
-
-            // $tab_name = $title
 
             ob_start();
+            
+            // Dołączamy nagłówek
+            if (file_exists($headPath)) {
+                include $headPath;
+            }
+
+            // Jeśli to nie jest login, dołączamy nawigację
+            if ($template !== 'login' && file_exists($navPath)) {
+                include $navPath;
+            }
+
             include $templatePath;
+
+            // Domykamy tagi, jeśli to nie login
+            if ($template !== 'login') {
+                echo '    </main>'; 
+                echo '</div>';      
+            }
+            
+            echo '</body></html>';
+            
             $output = ob_get_clean();
         } else {
-            ob_start();
-            include $templatePath404;
-            $output = ob_get_clean();
+            // Prosta informacja o braku pliku dla debugowania
+            $output = "Błąd: Nie znaleziono pliku widoku: " . $templatePath;
         }
+        
         echo $output;
     }
-
 }
--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,11 +1,14 @@
 <?php
 
+// Ważne: DashboardController i AppController są w tym samym folderze, 
+// więc wystarczy zwykły require_once
 require_once 'AppController.php';
-class DashboardController extends AppController{
-    public function index(){
-        $title = "WDPAI - dashboard";
-        // TODO read data from db
+
+class DashboardController extends AppController {
+    public function index() {
+        $title = "OC Studio - Dashboard";
         
+        // Renderujemy widok 'dashboard'
         return $this->render('dashboard', ["title" => $title]);
     }
 }
